32fc73ad9a5fb476721b57073a16e97401cf87c5
James Snell <j@TwoZeroFive.dawning.ca>
2017-07-23 01:22:07 -0600

    Still trying to gain actual full intuitive understanding of quicksort. Insane. But I am making progress

--- Java/offroad-quicksort/Solution.java
+++ Java/offroad-quicksort/Solution.java
@@ -15,21 +15,44 @@ public class Solution {
             array[i] = in.nextInt();
         }
 
-	array = quicksort(array);
+	quicksort(array);
 
 	for (int i = 0; i < n; i++) {
         	System.out.println(array[i]+" ");
 	}
     }
 
-    private int[] quicksort(int[] array) {
-	return quicksort(array, 0, array.length-1);
+    private static void quicksort(int[] array) {
+	quicksort(array, 0, array.length-1);
     }
 
-    private int[] quicksort(int[] array, int left, int right) {
-	int pivot = (right-left)/2;
-	if (left >= right) return array;
+    private static void quicksort(int[] array, int left, int right) {
+	int pivot = (right+left)/2;
+	int pivotValue = array[pivot];
 
+	if (left >= right) return;
 
+	int i = left;
+	int j = right;
+
+	while (i<=j) {
+		while (array[i]<pivotValue) {
+			i++;
+		}
+		while (array[j]>pivotValue) {
+			j--;
+		}
+
+		if (i <= j) {
+			int temp = array[j];
+			array[j] = array[i];
+			array[i] = temp;
+			i++;
+			j--;
+		}
+	}
+
+	quicksort(array, left, j);
+	quicksort(array, i, right);
     }
 }
