c0bc8ab13b6410e191c5f00f5ff61546369af643
James Snell <j@TwoZeroFive.dawning.ca>
2017-07-24 23:43:26 -0600

    Yay, solved

--- Python/python_designer-door-mat/Solution.py
+++ Python/python_designer-door-mat/Solution.py
@@ -3,9 +3,9 @@
 #This is a snippet, so it's not intended to be run directly
 
 N, M = map(int,raw_input().split()) # More than 6 lines of code will result in 0 score. Blank lines are not counted.
-for i in xrange(1,N,2): 
-    print "-"*(M-((3*i)+9)) + ".|" + "..|"*(i-1) + "." + "-"*((M/2)-(3**(i-1)))
+for i in xrange(1,N,2):
+    print "-"*(((M/2) - ((3*i)/2))) + ".|" + "..|"*(i-1) + "." + "-"*(((M/2) - ((3*i)/2)))
 print ((M-15)/2)*"-"+"----WELCOME----"+((M-15)/2)*"-"
 for i in xrange(N-2,-1,-2): 
-    print "2 - "+i.__str__()
+    print "-"*(((M/2) - ((3*i)/2))) + ".|" + "..|"*(i-1) + "." + "-"*(((M/2) - ((3*i)/2)))
 
