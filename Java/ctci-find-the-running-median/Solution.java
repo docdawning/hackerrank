14d778c0280238f2a7f1b0eb826a8f590777d99f
James Snell <j@TwoZeroFive.dawning.ca>
2017-08-03 00:35:23 -0600

    Added stub for ctci find running median with heaps

---
+++ Java/ctci-find-the-running-median/Solution.java
@@ -0,0 +1,25 @@
+//Refs: https://www.hackerrank.com/challenges/ctci-find-the-running-median
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	public static void main(String[] args) {
+		Solution solution = new Solution(System.in);
+		solution.begin();
+	}
+
+	private void begin() {
+	        Scanner in = new Scanner(System.in);
+        	int n = in.nextInt();
+	        int[] a = new int[n];
+        	for(int a_i=0; a_i < n; a_i++){
+	            a[a_i] = in.nextInt();
+        	}
+	}
+    }
+}
+
