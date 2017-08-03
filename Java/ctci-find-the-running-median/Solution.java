511efd194171b6b5e43587bfee4e0f694d378597
James Snell <j@TwoZeroFive.dawning.ca>
2017-08-03 00:44:27 -0600

    Going to bed. Not much progress, but you know..

--- Java/ctci-find-the-running-median/Solution.java
+++ Java/ctci-find-the-running-median/Solution.java
@@ -5,14 +5,26 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
+import java.lang.Integer;
+import java.util.ArrayList;
 
 public class Solution {
+	ArrayList<Integer> minHeap;
+	ArrayList<Integer> maxHeap;
+
 	public static void main(String[] args) {
 		Solution solution = new Solution(System.in);
 		solution.begin();
 	}
 
+	private int getParentIndex(int index) { return ((index-2)/2); }
+	private int getLeftChildIndex(int index) { return ((index*2)+1); }
+	private int getRightChildIndex(int index) { return ((index*2)+2); }
+
 	private void begin() {
+		minHeap = new ArrayList<Integer>();
+		maxHeap = new ArrayList<Integer>();
+
 	        Scanner in = new Scanner(System.in);
         	int n = in.nextInt();
 	        int[] a = new int[n];
