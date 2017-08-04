3580b3ada4ef70cdc0fce0160471aa5f7beebb27
James Snell <j@TwoZeroFive.local>
2017-08-04 17:02:50 -0600

    Solved the basic conditionals for C++ challenge. Had some grief as I was using the wrong compiler. Hah\!

--- Cpp/c-tutorial-conditional-if-else/Solution.cpp
+++ Cpp/c-tutorial-conditional-if-else/Solution.cpp
@@ -20,13 +20,26 @@
 #include <iostream>
 #include <algorithm>
 #include <unordered_map>
-
 using namespace std;
 
+void report(string message) {
+	cout << message << "\n";
+}
 
 int main(){
-    int n;
-    cin >> n;
-    return 0;
+	int n;
+	cin >> n;
+	if (n == 1) report("one");
+	if (n == 2) report("two");
+	if (n == 3) report("three");
+	if (n == 4) report("four");
+	if (n == 5) report("five");
+	if (n == 6) report("six");
+	if (n == 7) report("seven");
+	if (n == 8) report("eight");
+	if (n == 9) report("nine"); 
+	if (9 < n) report("Greater than 9"); 
+
+	return 0;
 }
 
