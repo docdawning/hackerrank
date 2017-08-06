21b288ac5a3ebbf73b68e5070e18fc94fc94a88e
James Snell <j@Tesla.local>
2017-08-06 10:09:12 -0600

    Added C++ 'challenge' for writing functions

---
+++ Cpp/c-tutorial-functions/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+INPUT_FILE="stdin.txt"
+BINARY_FILE="a.out"
+
+if [ "$#" -eq 1 ] ; then
+	INPUT_FILE=$1
+fi
+
+if [ -f "$BINARY_FILE" ] ; then
+	echo "Removing old $BINARY_FILE file"
+	rm a.out
+fi
+
+g++ Solution.cpp
+if [ "$?" -eq 0 ] ; then 
+	cat $INPUT_FILE | ./$BINARY_FILE
+fi
