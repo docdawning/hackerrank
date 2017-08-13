c93b90b63b07433aae63a46ffae0df852dd62280
DocDawning <doc@dawning.ca>
2017-08-13 10:43:04 -0600

    Adding run script, should have been included a while ago

---
+++ Java/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+INPUT_FILE="stdin.txt"
+
+if [ "$#" -eq 1 ] ; then
+	INPUT_FILE=$1
+fi
+
+rm *.class
+javac Solution.java
+if [ "$?" -eq 0 ] ; then 
+	cat $INPUT_FILE | java Solution
+fi
