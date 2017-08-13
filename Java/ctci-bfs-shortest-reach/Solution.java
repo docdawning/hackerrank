8b9127f4cb5405b664e3b189f54c2b1be4352e0e
DocDawning <doc@dawning.ca>
2017-08-12 18:48:01 -0600

    Figured out logical flaw in seach depth tracking. Now only fails one test case, for being too slow, though it seems pretty fast when I run that case on my end

--- Java/ctci-bfs-shortest-reach/Solution.java
+++ Java/ctci-bfs-shortest-reach/Solution.java
@@ -16,10 +16,18 @@ public class Solution {
 		}
 	}
 
+	class SearchRecord {
+		Node node;
+		int depth;
+
+		SearchRecord(Node node, int depth) {
+			this.node = node;
+			this.depth = depth;
+		}
+	}
+
 	private static int EDGE_WEIGHT = 6;
-	private static boolean debugging = true;
 	HashMap<Integer,Node> nodeMap;
-	HashSet<Integer> nodesVisitedBySearch;
 	LinkedList<Integer> searchQueue;
 
 	public static void main(String[] args) {
@@ -63,7 +71,6 @@ public class Solution {
 		}
 
 		int idS = scanner.nextInt();
-		//if (debugging) System.out.println("Graph assembled. There are "+nodeMap.size()+" nodes. Starting node is "+idS);
 
 		printShortestDistances(idS);
 	}
@@ -74,7 +81,6 @@ public class Solution {
 		for (Node each : nodeMap.values()) {
 			if (each.id == idS) continue;
 
-			nodesVisitedBySearch = new HashSet<Integer>();
 			searchQueue = new LinkedList<Integer>();
 			int shortestDistance = getShortestDistanceBFS(nodeS, each);
 			System.out.print(shortestDistance+" ");
@@ -83,34 +89,26 @@ public class Solution {
 	}
 
 	private int getShortestDistanceBFS(Node src, Node dest) {
-		LinkedList<Node> nextToVisit = new LinkedList<Node>();
-		LinkedList<Integer> depthQueue = new LinkedList<Integer>();
+		LinkedList<SearchRecord> nextToVisit = new LinkedList<SearchRecord>();
 		HashSet<Integer> nodesVisited = new HashSet<Integer>();
 
-		
-		int depthIterator = 0;
-		nextToVisit.add(src);
-		depthQueue.add(new Integer(depthIterator));
+		nextToVisit.add(new SearchRecord(src, 0));
 
 		while (!nextToVisit.isEmpty()) {
-			Node next = nextToVisit.removeFirst();
-			Integer depth = depthQueue.removeFirst();
+			SearchRecord next = nextToVisit.removeFirst();
 
-			if (next.id == dest.id) {
-				return EDGE_WEIGHT*depth.intValue();
+			if (next.node.id == dest.id) {
+				return EDGE_WEIGHT*next.depth;
 			}
-
-			if (nodesVisited.contains(new Integer(next.id))) continue;
-			nodesVisited.add(new Integer(next.id));
 		
+			if (nodesVisited.contains(new Integer(next.node.id))) continue;
+			nodesVisited.add(new Integer(next.node.id));
+	
 			//add neighbours of this node
-			int addedCount = 0;
-			depthIterator++;
-			for (Node neighbour : next.neighbours) {
-				addedCount++;
-				nextToVisit.add(neighbour);
-				depthQueue.add(new Integer(depthIterator));
+			for (Node neighbour : next.node.neighbours) {
+				nextToVisit.addLast(new SearchRecord(neighbour, next.depth+1));
 			}
+
 		}
 
 		return -1;
