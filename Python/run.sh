c4f277966c976db89ac2fa9916d69970b060ed85
James Snell <j@Tesla.local>
2017-09-08 14:59:41 -0600

    Refactored Python projects to differentiate Python 2 and Python 3.

--- Python/run.sh
+++ Python/run.sh
@@ -5,8 +5,12 @@ if [ "$#" -eq 1 ] ; then
 	INPUT_FILE=$1
 fi
 
-if [ "$?" -eq 0 ] ; then 
-	cat $INPUT_FILE | python Solution.py
+if [ "$?" -eq 0 ] ; then
+	if [ -f "$INPUT_FILE" ] ; then
+		cat $INPUT_FILE | python Solution.py
+	else
+		python Solution.py
+	fi
 fi
 
 echo -e "\n"
