e74ef18c220f787a06d4bf70725b7d8f2e39ce9d
James Snell <j@Tesla.local>
2017-09-08 15:11:14 -0600

    Added simple Py2 conditional challenge

---
+++ Python/Python2/py-if-else/Solution.py
@@ -0,0 +1,13 @@
+#Refs: https://www.hackerrank.com/challenges/py-if-else/problem
+
+if __name__ == '__main__':
+	n = int(raw_input())
+	if (n % 2 != 0):
+		print "Weird"
+	else:
+		if (n in range(2,6) or n>20):
+			print "Not Weird"
+		if (n in range(6,21)):
+			print "Weird"
+			
+
